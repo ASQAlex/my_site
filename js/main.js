5d3bcfce8e0090f7293d048ed46930ad472c0c3f
Oleksii Kozoriezov <72416314+ASQAlex@users.noreply.github.com>
2024-10-23 12:38:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,3 +52,56 @@ var swiper = new Swiper(".mySwiper", {
     this.classList.toggle("change");
     document.getElementById("menu-top").classList.toggle("show");
 });
+
+
+// Ссылки на файлы с переводами
+const translations = {
+  en: '../translations/translations_en.json',
+  ua: '../translations/translations_ua.json',
+  ru: '../translations/translations_ru.json'
+};
+
+// Функция для загрузки и применения перевода
+function switchLanguage(lang) {
+  loadTranslations(translations[lang]);
+}
+
+// Загрузка JSON файла с переводом
+function loadTranslations(url) {
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          // Обновление текста на странице
+          document.getElementById('about-link').textContent = data.about_me;
+          document.getElementById('skills-link').textContent = data.skills;
+          document.getElementById('works-link').textContent = data.my_work;
+          document.getElementById('contacts-link').textContent = data.contacts;
+
+          document.getElementById('contacts__link-title').textContent = data.contacts;
+          document.getElementById('works__link-title').textContent = data.my_work;
+          document.getElementById('skills__link-title').textContent = data.skills;
+          document.getElementById('about__title').textContent = data.about_me;
+          document.getElementById('about-text-top').textContent = data.about_text_top;
+          document.getElementById('about-text-center').textContent = data.about_text_center;
+          document.getElementById('about-text-down').textContent = data.about_text_down;
+
+          document.getElementById('name-placeholder').setAttribute('placeholder', data.name_placeholder);
+          document.getElementById('email-placeholder').setAttribute('placeholder', data.email_placeholder);
+          document.getElementById('message-placeholder').setAttribute('placeholder', data.message_placeholder);
+          document.getElementById('send-message-btn').textContent = data.send_message;
+      })
+      .catch(error => console.error('Ошибка загрузки перевода:', error));
+}
+
+// Event listener для смены языка
+document.getElementById('language').addEventListener('change', function() {
+  const selectedLang = this.value;
+  switchLanguage(selectedLang);
+});
+
+// Загрузка языка по умолчанию
+document.addEventListener('DOMContentLoaded', () => {
+  const defaultLang = 'en';  // язык по умолчанию
+  document.getElementById('language').value = defaultLang;
+  loadTranslations(translations[defaultLang]);
+});
