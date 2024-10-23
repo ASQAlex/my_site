a93ffbd3c53bfca45f1bde7dee698592b4a7296f
Oleksii Kozoriezov <72416314+ASQAlex@users.noreply.github.com>
2024-10-23 12:55:53 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -54,24 +54,20 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
-// Ссылки на файлы с переводами
 const translations = {
-  en: '../translations/translations_en.json',
-  ua: '../translations/translations_ua.json',
-  ru: '../translations/translations_ru.json'
+  en: 'translations/translations_en.json',
+  ua: 'translations/translations_ua.json',
+  ru: 'translations/translations_ru.json'
 };
 
-// Функция для загрузки и применения перевода
 function switchLanguage(lang) {
   loadTranslations(translations[lang]);
 }
 
-// Загрузка JSON файла с переводом
 function loadTranslations(url) {
   fetch(url)
       .then(response => response.json())
       .then(data => {
-          // Обновление текста на странице
           document.getElementById('about-link').textContent = data.about_me;
           document.getElementById('skills-link').textContent = data.skills;
           document.getElementById('works-link').textContent = data.my_work;
@@ -90,18 +86,15 @@ function loadTranslations(url) {
           document.getElementById('message-placeholder').setAttribute('placeholder', data.message_placeholder);
           document.getElementById('send-message-btn').textContent = data.send_message;
       })
-      .catch(error => console.error('Ошибка загрузки перевода:', error));
+      .catch(error => console.error(error));
 }
-
-// Event listener для смены языка
 document.getElementById('language').addEventListener('change', function() {
   const selectedLang = this.value;
   switchLanguage(selectedLang);
 });
 
-// Загрузка языка по умолчанию
 document.addEventListener('DOMContentLoaded', () => {
-  const defaultLang = 'en';  // язык по умолчанию
+  const defaultLang = 'en'; 
   document.getElementById('language').value = defaultLang;
   loadTranslations(translations[defaultLang]);
 });
